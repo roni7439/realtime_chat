c0e5e7d236e81d7162d05ca29f7f00421b17f1bd
Washim Molla <ronimolla7439@gmail.com>
2025-07-15 15:00:56 +0530

    Add CSRF trusted origins for Render

--- myproject/settings.py
+++ myproject/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = ['https://realtime-chat-1-6ro9.onrender.com']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
